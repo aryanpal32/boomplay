23f9c022091723acce91284cac16c8b4c7c48627
aryanpal32 <boomplaymediacasting@gmail.com>
2025-07-12 01:52:26 +0530

    SEO added

--- components/SEO.js
+++ components/SEO.js
@@ -48,10 +48,11 @@ const SEO = ({
       )}
       
       {/* Favicons */}
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-      <link rel="manifest" href="/favicons/site.webmanifest" />
+  <link rel="icon" href="/favicon.ico" sizes="any" />
+<link rel="icon" href="/favicons/favicon-32x32.png" type="image/png" />
+<link rel="icon" href="/favicons/favicon-16x16.png" type="image/png" />
+<link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+<link rel="manifest" href="/favicons/site.webmanifest" />
     </Head>
   );
 };
