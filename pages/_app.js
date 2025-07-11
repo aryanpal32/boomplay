bbee8b6139885bcbe16984ebdbc80bb3486f015f
aryanpal32 <boomplaymediacasting@gmail.com>
2025-07-12 01:57:24 +0530

    SEO added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
+
+export default MyApp;
